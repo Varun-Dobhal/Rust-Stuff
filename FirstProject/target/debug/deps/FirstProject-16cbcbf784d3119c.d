7c5c74e1ae007bdf40ef3f143378eb2274b78620
Varun-Dobhal <varundobhal33@gmail.com>
2025-09-02 01:00:46 +0530

    Starting Rust today!

---
+++ FirstProject/target/debug/deps/FirstProject-16cbcbf784d3119c.d
@@ -0,0 +1,5 @@
+C:\Users\varun\Desktop\Rust Stuff\FirstProject\target\debug\deps\FirstProject-16cbcbf784d3119c.d: src\main.rs
+
+C:\Users\varun\Desktop\Rust Stuff\FirstProject\target\debug\deps\libFirstProject-16cbcbf784d3119c.rmeta: src\main.rs
+
+src\main.rs:
---
+++ FirstProject/target/debug/deps/FirstProject-742363ac45de53d9.d
@@ -0,0 +1,5 @@
+C:\Users\varun\Desktop\Rust Stuff\FirstProject\target\debug\deps\FirstProject-742363ac45de53d9.d: src\main.rs
+
+C:\Users\varun\Desktop\Rust Stuff\FirstProject\target\debug\deps\libFirstProject-742363ac45de53d9.rmeta: src\main.rs
+
+src\main.rs:
