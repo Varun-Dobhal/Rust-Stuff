497986e726021b7b8c6ff956663304012c91568f
Dobhal <varundobhal33@gmail.com>
2025-10-10 12:08:05 +0530

    Vectors

--- FirstProject/src/main.rs
+++ FirstProject/src/main.rs
@@ -8,6 +8,8 @@
 //     let w=num+1;
 // }
 
+use std::vec;
+
 // fn main() {
 //     let n=5;
 //     let mut fact=1;
@@ -23,18 +25,41 @@ fn main(){
     // println!("{x}");
     // let x=[1,3,4,2,4];
     // let slice=&x[2..4];
-    struct Coffee{
-        name:String,
-        price:i32,
-    }
-    let mocha=Coffee{
-        name:String::from("Mocha"),
-        price:500,
-    };
-    print!("{}",mocha.name);
+    // struct Coffee{
+    //     name:String,
+    //     price:i32,
+    // }
+    // let mocha=Coffee{
+    //     name:String::from("Mocha"),
+    //     price:500,
+    // };
+    // print!("{}",mocha.name);
+    
+    // println!("Value is {}",identity(5));
+    // println!("Value is {}",identity(3.14));
+
+    // let str_arr=[
+    //     String::from("Hlo"),
+    //     String::from("Meow"),
+    //     String::from("Boom"),
+    // ];
+    // let b=str_arr.get(2);
+    // println!("{b:?}");
+
+    // let mut s=vec!["Hlo","Hi","Bi"];
+    // while let Some(s) = s.pop() {
+    //     println!("{s:?}");
+    // }
+
+    let x= vec!["ho","yo"];
+    let xx=x[0];
+    println!("{xx}");
 
 
 }
 // fn fun(mut meal:String){
 //     meal.push_str(" and fries");
+// }
+// fn identity<T>(value:T)->T{
+//     value
 // }
